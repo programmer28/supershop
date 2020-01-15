2e23028e928ac9900cf172daa36214d47d5a93be
Yaroslav <yaroslav2875@i.ua>
2020-01-15 17:24:19 +0200

    added hw 18.2 solution made defined methods with httpSession

--- src/main/java/mate/academy/internetshop/controller/AddItemToBucketController.java
+++ src/main/java/mate/academy/internetshop/controller/AddItemToBucketController.java
@@ -10,7 +10,6 @@ import mate.academy.internetshop.model.Bucket;
 import mate.academy.internetshop.model.Item;
 import mate.academy.internetshop.service.BucketService;
 import mate.academy.internetshop.service.ItemService;
-import mate.academy.internetshop.service.UserService;
 
 public class AddItemToBucketController extends HttpServlet {
 
@@ -19,9 +18,6 @@ public class AddItemToBucketController extends HttpServlet {
 
     @Inject
     private static BucketService bucketService;
-
-    @Inject
-    private static UserService userService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
